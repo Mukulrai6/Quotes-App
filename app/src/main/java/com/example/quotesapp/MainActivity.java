dd396e3b5deb5a875aa4d9adc8c2a2ee577a98e0
mukulrai6 <mukulray44@gmail.com>
2024-07-20 17:52:31 +0530

    1.0 Commit

---
+++ app/src/main/java/com/example/quotesapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.quotesapp;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button shareBtn;
+    TextView quote;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        shareBtn = findViewById(R.id.shareBtn);
+        quote = findViewById(R.id.tvquote);
+
+        shareBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sharetxt = quote.getText().toString();
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                sendIntent.putExtra(Intent.EXTRA_TEXT,sharetxt);
+                startActivity(sendIntent);
+            }
+        });
+
+    }
+}
